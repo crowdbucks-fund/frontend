086cf73ef46d10204d297cb0dff3f31b69488cf9
Crowdbucks Frontend <218576460+crowdbucks-frontend@users.noreply.github.com>
2025-07-18 01:24:38 +0330

    feat: redesign not-found page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,6 +1,7 @@
 "use client";
-import { Button, Text, VStack } from "@chakra-ui/react";
-import img from "assets/images/not-found.png";
+import { Box, Button, Text, VStack } from "@chakra-ui/react";
+import emptyWallet1 from "assets/images/empty-wallet-1.png";
+import emptyWallet2 from "assets/images/empty-wallet-2.png";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -20,44 +21,105 @@ export default function notFoundPage() {
       bg="brand.gray.3"
       px={4}
     >
-      <Image
-        priority
-        src="/logo.svg"
-        alt="CrowdBucks Logo"
-        width={188}
-        height={42}
-      />
-      <VStack bg="brand.gray.4" maxW="630px" w="full" rounded="24px" p={8}>
+      <Box
+        __css={{
+          "& > img": { maxW: { base: "140px", md: "full" } },
+        }}
+      >
+        <Image
+          as={Image}
+          priority
+          src="/logo.svg"
+          alt="CrowdBucks Logo"
+          width={188}
+          height={42}
+        />
+      </Box>
+      <VStack
+        bg="brand.gray.4"
+        maxW={{
+          base: "310px",
+          md: "630px",
+        }}
+        w="full"
+        rounded="24px"
+        p={{
+          base: 5,
+          md: 8,
+        }}
+        position="relative"
+      >
         <VStack
-          maxW="400px"
+          w="full"
           gap={{
             base: 10,
             md: 14,
           }}
         >
-          <Image src={img} alt="" priority />
-          <VStack gap="4">
+          <Box
+            position="absolute"
+            top="0"
+            left="0"
+            __css={{
+              "& > img": {
+                maxW: {
+                  base: "140px",
+                  md: "full",
+                },
+              },
+            }}
+          >
+            <Image src={emptyWallet2} alt="" priority />
+          </Box>
+          <Box
+            position="absolute"
+            top="0"
+            right="0"
+            __css={{
+              "& > img": {
+                maxW: {
+                  base: "100px",
+                  md: "full",
+                },
+              },
+            }}
+          >
+            <Image src={emptyWallet1} alt="" priority />
+          </Box>
+          <VStack
+            gap="4"
+            minH={{ base: "270px", md: "450px" }}
+            justify="end"
+            alignItems="start"
+            w="full"
+          >
             <Text
               fontWeight="bold"
               fontSize={{
-                base: "16px",
-                md: "30px",
+                base: "30px",
+                md: "58px",
               }}
             >
-              Page Not Found!
+              404
             </Text>
-            <Link href="/">
-              <Button
-                variant="solid"
-                colorScheme="primary"
-                size={{
-                  lg: "lg",
-                  base: "sm",
-                }}
-              >
-                Back to Home
-              </Button>
-            </Link>
+            <Text
+              fontSize={{
+                base: "16px",
+                md: "20px",
+              }}
+            >
+              There’s Nothing Here
+            </Text>
+            <Button
+              w="full"
+              variant="solid"
+              colorScheme="black"
+              size="lg"
+              as={Link}
+              href="/"
+            >
+              Go Back To Main Page
+            </Button>
           </VStack>
         </VStack>
       </VStack>
