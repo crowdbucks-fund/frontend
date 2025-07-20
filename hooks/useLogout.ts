2877481795f91e40f34c6d52b7c47b18fdcfdeb8
Crowdbucks Frontend <218576460+crowdbucks-frontend@users.noreply.github.com>
2025-07-20 19:05:49 +0330

    feat: implement auth token management and improve community link generation

--- hooks/useLogout.ts
+++ hooks/useLogout.ts
@@ -3,7 +3,6 @@ import { queryClient } from "lib/reactQuery";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import { useMutation } from "react-query";
 
-export const logout = (router: AppRouterInstance) => { };
 
 export const useLogout = (router: AppRouterInstance) =>
   useMutation(() => {
