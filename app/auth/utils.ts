653913ce8e8efdee17b308d55732c86a36567201
Crowdbucks Frontend <218576460+crowdbucks-frontend@users.noreply.github.com>
2025-07-20 13:53:56 +0330

    refactor: simplify key generation for instance credentials in utils

--- app/auth/utils.ts
+++ app/auth/utils.ts
@@ -5,6 +5,7 @@ import { cookies } from "next/headers";
 import { hash } from 'ohash';
 import invariant from "tiny-invariant";
 import { stringifyParsedURL, withQuery } from "ufo";
+
 export const getRedirectUrl = (headers: ReadonlyHeaders, oauthCallbackPath: string, step?: string) => {
   const host = headers.get("host")!
   return withQuery(stringifyParsedURL({
@@ -15,7 +16,6 @@ export const getRedirectUrl = (headers: ReadonlyHeaders, oauthCallbackPath: stri
     step,
   })
 };
-
 
 export const serializeOauthStateCookie = async () => {
   const cookie = await cookies()
@@ -31,11 +31,19 @@ export const deleteOauthStateCookie = async () => {
 
 export const getInstanceCredentials = async (instance: string, callbackUrl: string) => {
   const kv = getCloudflareContext().env.OAUTH_KV;
-  const credentials = await kv.get<{ client_id: string, client_secret: string }>(`instance_${hash(callbackUrl)}_${instance}`.toLowerCase(), "json");
+  const key = `instance_${hash({
+    instance,
+    callbackUrl
+  })}`
+  const credentials = await kv.get<{ client_id: string, client_secret: string }>(key.toLowerCase(), "json");
   return credentials;
 }
 
 export const storeInstanceCredentials = async (instance: string, callbackUrl: string, credentials: { client_id: string, client_secret: string }) => {
   const kv = getCloudflareContext().env.OAUTH_KV;
-  await kv.put(`instance_${hash(callbackUrl)}_${instance}`, JSON.stringify(credentials));
+  const key = `instance_${hash({
+    instance,
+    callbackUrl
+  })}`
+  await kv.put(key, JSON.stringify(credentials));
 }
