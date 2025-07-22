0ad741e79135e7aacf002bfc36b559f5f903b981
Crowdbucks Frontend <218576460+crowdbucks-frontend@users.noreply.github.com>
2025-07-22 11:00:07 +0330

    refactor: await params on tiers page

--- app/c/[community]/(community-info)/_delete_soon/[goal]/page.tsx
+++ app/c/[community]/(community-info)/_delete_soon/[goal]/page.tsx
@@ -1,19 +1,24 @@
-import { api } from 'lib/api.server'
-import { notFound } from 'next/navigation'
-import GoalClientPage from './page.client'
+import { api } from "lib/api.server";
+import { notFound } from "next/navigation";
+import GoalClientPage from "./page.client";
 
 const findGoal = async (communityHandle: string, goalId: number) => {
   try {
     return await api.confirmCommunityGoalExists({
       communityHandle,
       goalId,
-    })
+    });
   } catch (e) {
-    return null
+    return null;
   }
-}
-export default async function GoalPage({ params: { community, goal } }: { params: { community: string; goal: string } }) {
-  const currentGoal = await findGoal(community, parseInt(goal))
-  if (currentGoal && currentGoal.belongsToCommunity) return <GoalClientPage goal={currentGoal} />
-  return notFound()
+};
+export default async function GoalPage({ params }: { params: Params }) {
+  const { community, goal } = await params;
+  const currentGoal = await findGoal(
+    community.toString(),
+    parseInt(goal.toString())
+  );
+  if (currentGoal && currentGoal.belongsToCommunity)
+    return <GoalClientPage goal={currentGoal} />;
+  return notFound();
 }
--- app/c/[community]/(community-info)/tiers/[tier]/page.tsx
+++ app/c/[community]/(community-info)/tiers/[tier]/page.tsx
@@ -1,19 +1,24 @@
-import { api } from 'lib/api.server'
-import { notFound } from 'next/navigation'
-import TierClientPage from './page.client'
+import { api } from "lib/api.server";
+import { notFound } from "next/navigation";
+import TierClientPage from "./page.client";
 
 const findTier = async (communityHandle: string, tierId: number) => {
   try {
     return await api.confirmCommunityTierExists({
       communityHandle,
       tierId,
-    })
+    });
   } catch (e) {
-    return null
+    return null;
   }
-}
-export default async function TierPage({ params: { community, tier } }: { params: { community: string; tier: string } }) {
-  const currentTier = await findTier(community, parseInt(tier))
-  if (currentTier && currentTier.belongsToCommunity) return <TierClientPage tier={currentTier} />
-  return notFound()
+};
+export default async function TierPage({ params }: { params: Params }) {
+  const { community, tier } = await params;
+  const currentTier = await findTier(
+    community.toString(),
+    parseInt(tier.toString())
+  );
+  if (currentTier && currentTier.belongsToCommunity)
+    return <TierClientPage tier={currentTier} />;
+  return notFound();
 }
