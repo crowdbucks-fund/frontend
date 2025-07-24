4ba0c908f600374a7c97f45867c2e52efcda9f2c
Crowdbucks Frontend <218576460+crowdbucks-frontend@users.noreply.github.com>
2025-07-24 18:03:49 +0330

    feat: allow api calls on production

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import API from '@xeronith/granola'
 
 export const api = new API(process.env.NEXT_PUBLIC_API_BASE as string, {
-  suppressConsoleOutput: process.env.NODE_ENV === 'production',
+  // suppressConsoleOutput: process.env.NODE_ENV === 'production',
 })
 export type ApiError = { message: string }
