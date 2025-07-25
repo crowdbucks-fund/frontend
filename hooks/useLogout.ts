c7ea1790f77415ace8ec6991edba6e3c9afd135a
Crowdbucks Frontend <218576460+crowdbucks-frontend@users.noreply.github.com>
2025-07-26 02:57:32 +0330

    feat: authorizing button loading state + improved email input layout

--- hooks/useLogout.ts
+++ hooks/useLogout.ts
@@ -1,13 +1,16 @@
+'use client'
+import { logout } from "hooks/useLogout.server";
 import { api } from "lib/api";
 import { queryClient } from "lib/reactQuery";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import { useMutation } from "react-query";
 
-
 export const useLogout = (router: AppRouterInstance) =>
-  useMutation(() => {
-    return api.logout({}).finally(() => {
-      queryClient.clear()
-      router.push("/auth/logout");
-    });
+  useMutation({
+    mutationFn: async () => {
+      return Promise.allSettled([logout(), api.logout({})]).finally(() => {
+        queryClient.clear()
+        router.push("/auth/logout");
+      })
+    }
   });
