e486057b44708651d037e1ab274cf54d6543e7ed
Crowdbucks Frontend <218576460+crowdbucks-frontend@users.noreply.github.com>
2025-07-25 21:26:29 +0330

    feat: tiers order summery

--- app/c/[community]/(community-info)/tiers/[tier]/page.client.tsx
+++ app/c/[community]/(community-info)/tiers/[tier]/page.client.tsx
@@ -1,10 +1,19 @@
 "use client";
-import { CircularProgress, HStack } from "@chakra-ui/react";
+import {
+  Box,
+  CircularProgress,
+  Divider,
+  Flex,
+  HStack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { UserTier } from "@xeronith/granola/core/objects";
 import { useCurrentCommunity } from "app/console/communities/[community]/components/community-validator-layout";
 import { CenterLayout } from "app/console/components/CenterLayout";
 import { PaymentForm } from "components/PaymentForm";
 import { useCreateStripeIntent } from "hooks/useCreateStripeIntent";
+import { lowerCase } from "lodash";
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 import { useUpdateBreadcrumb } from "states/console/breadcrumb";
@@ -67,22 +76,86 @@ export default function TierClientPage({ tier }: { tier: UserTier }) {
         flexGrow: 1,
         height: "full",
       }}
-      maxW={{ base: "unset", md: "370px" }}
+      maxW="full"
+      justifyContent="start"
     >
-      {isLoading && (
-        <HStack justify="center">
-          <CircularProgress isIndeterminate color="secondary.500" size="40px" />
-        </HStack>
-      )}
-      {paymentFormReady && (
-        <PaymentForm
-          {...paymentFormData}
-          returnUrl={`${joinURL(
-            window.location.origin,
-            getCommunityTiersLink(community)
-          )}?verify`}
-        />
-      )}
+      <Flex
+        flexDirection={{
+          base: "column",
+          md: "row",
+        }}
+        w="full"
+        h="full"
+        gap={8}
+        justifyContent="center"
+      >
+        <Box
+          maxW={{
+            base: "unset",
+            md: "340px",
+          }}
+          flexGrow={1}
+        >
+          <VStack
+            p={{ base: 4, md: 5 }}
+            borderRadius="2xl"
+            border="2px solid"
+            borderColor="brand.gray.1"
+            bg="brand.gray.4"
+            gap={4}
+            w="full"
+            alignItems="start"
+          >
+            <Text
+              fontSize={{
+                base: "16px",
+                md: "18px",
+              }}
+              fontWeight="bold"
+            >
+              Order Summary
+            </Text>
+            <Divider color="brand.gray.2" />
+            <VStack w="full">
+              <HStack w="full" justifyContent="space-between">
+                <Text>Price</Text>
+                <Text>
+                  ${tier.amount}/{lowerCase(tier.tierFrequency?.unit)}
+                </Text>
+              </HStack>
+            </VStack>
+            <Divider color="brand.gray.2" />
+            <VStack w="full">
+              <HStack w="full" justifyContent="space-between">
+                <Text>Total</Text>
+                <Text>
+                  ${tier.amount}/{lowerCase(tier.tierFrequency?.unit)}
+                </Text>
+              </HStack>
+            </VStack>
+          </VStack>
+        </Box>
+        <VStack minW={{ base: "unset", md: "370px" }}>
+          {isLoading && (
+            <HStack justify="center">
+              <CircularProgress
+                isIndeterminate
+                color="secondary.500"
+                size="40px"
+              />
+            </HStack>
+          )}
+          {paymentFormReady && (
+            <PaymentForm
+              {...paymentFormData}
+              returnUrl={`${joinURL(
+                window.location.origin,
+                getCommunityTiersLink(community)
+              )}?verify`}
+            />
+          )}
+        </VStack>
+      </Flex>
     </CenterLayout>
   );
 }
