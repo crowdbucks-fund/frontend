15039e19c4239ba72af47dae977aeaad8d2ea982
Crowdbucks Frontend <218576460+crowdbucks-frontend@users.noreply.github.com>
2025-07-26 13:31:24 +0330

    feat: move logout button to more options bottom sheet

--- app/console/components/MoreOptionsBottomDrawer.tsx
+++ app/console/components/MoreOptionsBottomDrawer.tsx
@@ -13,60 +13,65 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { ChevronRightIcon } from "@heroicons/react/24/outline";
+import { LogoutModal } from "app/console/edit-profile/LogoutModal";
+import LogoutIcon from "assets/icons/logout.svg?react";
 import UserEditIcon from "assets/icons/user-edit.svg?react";
 import defaultAvatar from "assets/images/default-profile.png";
 import { mobileSidebarMenu } from "constants/console";
 import { useUserAuthProvider } from "hooks/useUserAuthProvider";
 import { atom, useAtom } from "jotai";
 import NextLink from "next/link";
+import { useState } from "react";
 import { useAuth } from "states/console/user";
 
 export const moreDrawerState = atom(false);
 
 export const MoreOptionsBottomDrawer = () => {
   const [isMoreDrawerOpen, setIsMoreDrawerOpen] = useAtom(moreDrawerState);
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
   const { user } = useAuth();
   const authProvider = useUserAuthProvider();
+
   return (
-    <Drawer
-      onClose={setIsMoreDrawerOpen.bind(null, false)}
-      placement={"bottom"}
-      isOpen={isMoreDrawerOpen}
-    >
-      <DrawerOverlay />
-      <DrawerContent>
-        <DrawerBody>
-          <VStack py={6} gap={4}>
-            <HStack w="full">
-              <VStack w="full" gap={2}>
-                <Avatar
-                  as={NextLink}
-                  href="/console/edit-profile"
-                  width="70px"
-                  height="70px"
-                  src={user?.avatar || defaultAvatar.src}
-                  onClick={setIsMoreDrawerOpen.bind(null, false)}
-                />
-                <HStack>
-                  <IconButton
-                    aria-label="Edit User"
-                    variant="ghost"
+    <>
+      <Drawer
+        onClose={setIsMoreDrawerOpen.bind(null, false)}
+        placement={"bottom"}
+        isOpen={isMoreDrawerOpen}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerBody>
+            <VStack py={6} gap={4}>
+              <HStack w="full">
+                <VStack w="full" gap={2}>
+                  <Avatar
                     as={NextLink}
                     href="/console/edit-profile"
+                    width="70px"
+                    height="70px"
+                    src={user?.avatar || defaultAvatar.src}
                     onClick={setIsMoreDrawerOpen.bind(null, false)}
-                  >
-                    <UserEditIcon />
-                  </IconButton>
-                  <Text fontWeight="bold" fontSize="16px">
-                    {user?.displayName}
+                  />
+                  <HStack>
+                    <IconButton
+                      aria-label="Edit User"
+                      variant="ghost"
+                      as={NextLink}
+                      href="/console/edit-profile"
+                      onClick={setIsMoreDrawerOpen.bind(null, false)}
+                    >
+                      <UserEditIcon />
+                    </IconButton>
+                    <Text fontWeight="bold" fontSize="16px">
+                      {user?.displayName}
+                    </Text>
+                  </HStack>
+                  <Text color="gray.700" fontSize={"14px"}>
+                    {authProvider.provider}: {authProvider.value}
                   </Text>
-                </HStack>
-                <Text color="gray.700" fontSize={"14px"}>
-                  {authProvider.provider}: {authProvider.value}
-                </Text>
-              </VStack>
-            </HStack>
-            {mobileSidebarMenu.length > 0 && (
+                </VStack>
+              </HStack>
               <VStack w="full" gap={3}>
                 {mobileSidebarMenu.map((menuItem) => {
                   const Icon = menuItem.icon;
@@ -98,11 +103,33 @@ export const MoreOptionsBottomDrawer = () => {
                     </Button>
                   );
                 })}
+                <Button
+                  fontWeight="medium"
+                  fontSize="14px"
+                  bg="red.50"
+                  colorScheme="red"
+                  color="red.500"
+                  w="full"
+                  py="6"
+                  justifyContent="start"
+                  variant="ghost"
+                  rounded="10px"
+                  leftIcon={<LogoutIcon width="18px" />}
+                  title="Log out"
+                  aria-label="Log out"
+                  onClick={setIsLogoutModalOpen.bind(null, true)}
+                >
+                  Log out
+                </Button>
               </VStack>
-            )}
-          </VStack>
-        </DrawerBody>
-      </DrawerContent>
-    </Drawer>
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+      <LogoutModal
+        isOpen={isLogoutModalOpen}
+        onClose={setIsLogoutModalOpen.bind(null, false)}
+      />
+    </>
   );
 };
