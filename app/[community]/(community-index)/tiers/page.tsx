29df8c09aa628babfc6993c790c8f6c494ca7045
Crowdbucks Frontend <218576460+crowdbucks-frontend@users.noreply.github.com>
2025-07-28 14:11:02 +0330

    feat: donation success modal icon

--- app/[community]/(community-index)/tiers/page.tsx
+++ app/[community]/(community-index)/tiers/page.tsx
@@ -1,20 +1,21 @@
 "use client";
 
-import { Button, HStack, Text, VStack } from "@chakra-ui/react";
+import { Button, chakra, HStack, Text, VStack } from "@chakra-ui/react";
 import { FindCommunityByUserResult } from "@xeronith/granola/core/spi";
 import { EmptyState } from "app/[community]/(community-index)/EmptyState";
 import { useCurrentCommunity } from "app/console/communities/[community]/components/community-validator-layout";
 import { CenterLayout } from "app/console/components/CenterLayout";
-import createCommunityImage from "assets/images/amico.svg";
+import TickSquare from "assets/icons/tick-square.svg?react";
 import { ResponsiveDialog } from "components/ResponsiveDialog";
 import { TierCard } from "components/TierCard";
 import { usePaymentVerification } from "hooks/usePaymentVerification";
 import { sortTiers } from "hooks/useTiers.server";
-import Image from "next/image";
 import NextLink from "next/link";
 import { useUpdateBreadcrumb } from "states/console/breadcrumb";
 import { joinURL } from "ufo";
 import { getCommunityLink, getCommunityTiersLink } from "utils/community";
+
+const CheckIcon = chakra(TickSquare);
 
 export default function TierPage() {
   const community = useCurrentCommunity<FindCommunityByUserResult>();
@@ -36,6 +37,7 @@ export default function TierPage() {
     },
     showConsoleMenu: false,
   });
+
   return (
     <CenterLayout maxW={{ md: "630px" }} mx="auto" gap={{ base: 4, md: 8 }}>
       {isSuccess && (
@@ -50,20 +52,17 @@ export default function TierPage() {
           <VStack
             justify="center"
             textAlign="center"
-            gap={1}
+            gap={4}
             py={4}
             mt={{
               base: 0,
               md: 6,
             }}
           >
+            <CheckIcon color="primary.500" w={{ base: "42px", md: "52px" }} />
             <Text textStyle="modalTitle">
               Thanks a lot for your contribution
             </Text>
-            <Text textStyle="regular20" mb={4}>
-              Your donation helps {community.name} to grow
-            </Text>
-            <Image alt="" src={createCommunityImage} width={300} priority />
           </VStack>
         </ResponsiveDialog>
       )}
