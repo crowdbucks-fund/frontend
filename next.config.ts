87368b6ee585bbb01011197d08cf38cb556076d8
Crowdbucks Frontend <218576460+crowdbucks-frontend@users.noreply.github.com>
2025-09-19 02:33:45 +0330

    fix: integrate PostHog configuration into Next.js setup

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
+import { withPostHogConfig } from "@posthog/nextjs-config";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig = withPostHogConfig({
   reactStrictMode: false,
   experimental: {
     serverComponentsHmrCache: false,
@@ -159,7 +160,11 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-};
+}, {
+  personalApiKey: process.env.POSTHOG_API_KEY!, // Personal API Key
+  envId: process.env.POSTHOG_ENV_ID!, // Environment ID
+  host: process.env.NEXT_PUBLIC_POSTHOG_HOST, // (optional), defaults to https://us.posthog.com
+});
 
 export default nextConfig;
 
